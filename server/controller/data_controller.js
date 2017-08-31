cc9609596b44f576c9c5cac5ed976b69bd77e58d
apple2@ioioinfo.com <apple2@ioioinfo.com>
2017-08-31 09:55:49 +0800

    update

--- server/controller/data_controller.js
+++ server/controller/data_controller.js
@@ -48,13 +48,253 @@ exports.register = function(server, options, next) {
             handler: function(request, reply) {
                 api.get_departments(function(err,rows){
                     if (!err) {
-                        return reply({"success":true,"rows":rows});
+                        return reply(rows);
                     }else {
                         return reply({"success":false,"message":rows.message});
                     }
                 });
             },
         },
+        //保存部门
+        {
+            method: 'POST',
+            path: '/save_department',
+            handler: function(request, reply) {
+                var name = request.payload.name;
+                var code = request.payload.code;
+                var source_level = request.payload.source_level;
+                if (!name || !code || !source_level) {
+                    return reply({"success":false,"message":"params wrong","service_info":service_info});
+                }
+                var data = {
+                    "name":name,
+                    "code":code,
+                    "source_level":source_level
+                };
+
+                api.save_department(data,function(err,rows){
+                    if (!err) {
+                        return reply({"success":true,"message":rows.message});
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            },
+        },
+        //部门删除
+        {
+            method: "POST",
+            path: '/delete_department',
+            handler: function(request, reply) {
+                var id = request.payload.id;
+                if (!id) {
+                    return reply({"success":false,"message":"id wrong","service_info":service_info});
+                }
+                var data = {
+                    "id" : id
+                };
+                api.delete_department(data,function(err,rows){
+                    if (!err) {
+                        return reply(rows);
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            }
+        },
+        //根据id查找部门
+        {
+            method: "GET",
+            path: '/search_department_byId',
+            handler: function(request, reply) {
+                var id = request.query.id;
+                if (!id) {
+                    return reply({"success":false,"message":"id null","service_info":service_info});
+                }
+                api.search_department_byId(id,function(err,rows){
+                    if (!err) {
+                        return reply(rows);
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            }
+        },
+        //更新部门
+        {
+            method: "POST",
+            path: '/update_department',
+            handler: function(request, reply) {
+                var id = request.payload.id;
+                var name = request.payload.name;
+                var code = request.payload.code;
+                var source_level = request.payload.source_level;
+
+                if (!id || !name || !code || !source_level) {
+                    return reply({"success":false,"message":"params wrong","service_info":service_info});
+                }
+
+                var data = {
+                    "id" : id,
+                    "name" : name,
+                    "code" : code,
+                    "source_level" : source_level
+                };
+                api.update_department(data,function(err,rows){
+                    if (!err) {
+                        return reply(rows);
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            }
+        },
+        //查询所有渠道
+        {
+            method: 'GET',
+            path: '/get_channels',
+            handler: function(request, reply) {
+                api.get_channels(function(err,rows){
+                    if (!err) {
+                        return reply(rows);
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            },
+        },
+        //新增渠道
+        {
+            method: 'POST',
+            path: '/save_channel',
+            handler: function(request, reply) {
+                var name = request.payload.name;
+                var code = request.payload.code;
+                var department_id = request.payload.department_id;
+                if (!name || !code || !department_id) {
+                    return reply({"success":false,"message":"params wrong","service_info":service_info});
+                }
+                var data = {
+                    "name":name,
+                    "code":code,
+                    "department_id":department_id
+                };
+
+                api.save_channel(data,function(err,rows){
+                    if (!err) {
+                        return reply({"success":true,"message":rows.message});
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            },
+        },
+        //更新渠道
+        {
+            method: "POST",
+            path: '/update_channel',
+            handler: function(request, reply) {
+                var id = request.payload.id;
+                var name = request.payload.name;
+                var code = request.payload.code;
+                var department_id = request.payload.department_id;
+
+                if (!id || !name || !code || !department_id) {
+                    return reply({"success":false,"message":"params wrong","service_info":service_info});
+                }
+
+                var data = {
+                    "id" : id,
+                    "name" : name,
+                    "code" : code,
+                    "department_id" : department_id
+                };
+                api.update_channel(data,function(err,rows){
+                    if (!err) {
+                        return reply(rows);
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            }
+        },
+        //渠道删除
+        {
+            method: "POST",
+            path: '/delete_channel',
+            handler: function(request, reply) {
+                var id = request.payload.id;
+                if (!id) {
+                    return reply({"success":false,"message":"id wrong","service_info":service_info});
+                }
+                var data = {
+                    "id" : id
+                };
+                api.delete_channel(data,function(err,rows){
+                    if (!err) {
+                        return reply(rows);
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            }
+        },
+        //根据id查找渠道
+        {
+            method: "GET",
+            path: '/search_channel_byId',
+            handler: function(request, reply) {
+                var id = request.query.id;
+                if (!id) {
+                    return reply({"success":false,"message":"id null","service_info":service_info});
+                }
+                api.search_channel_byId(id,function(err,rows){
+                    if (!err) {
+                        return reply(rows);
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            }
+        },
+        //查询所有预算
+        {
+            method: 'GET',
+            path: '/get_budgets',
+            handler: function(request, reply) {
+                api.get_budgets(function(err,rows){
+                    if (!err) {
+                        return reply(rows);
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            },
+        },
+        //新增预算
+        {
+            method: 'POST',
+            path: '/save_budget',
+            handler: function(request, reply) {
+                var budget = request.payload.budget;
+                budget = JSON.parse(budget);
+                if (!budget.name || !budget.channel_id || !budget.employees_cost || !budget.locations_cost || !budget.materials_cost || !budget.medias_cost) {
+                    return reply({"success":false,"message":"params wrong","service_info":service_info});
+                }
+                var data = {
+                    "budget":JSON.stringify(budget)
+                };
+
+                api.save_budget(data,function(err,rows){
+                    if (!err) {
+                        return reply({"success":true,"message":rows.message});
+                    }else {
+                        return reply({"success":false,"message":rows.message});
+                    }
+                });
+            },
+        },
+
 
 
     ]);
--- server/services/ioio_sales_api.js
+++ server/services/ioio_sales_api.js
@@ -22,6 +22,130 @@ var host = "http://211.149.248.241:18028/";
 
 var nav = function(server) {
     return {
+        //保存预算
+        save_budget: function(data,cb) {
+            var url = host + "save_budget";
+            uu_request.request(url, data, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    // var info = JSON.parse(body);
+                    cb(err,body);
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
+        //查询所有预算
+        get_budgets: function(cb) {
+            var url = host + "get_budgets";
+            uu_request.get(url, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    cb(err,JSON.parse(body));
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
+        //根据id查找渠道
+        search_channel_byId: function(id,cb) {
+            var url = host + "search_channel_byId?id=" + id;
+            uu_request.get(url, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    cb(err,JSON.parse(body));
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
+        //渠道删除
+        delete_channel: function(data,cb) {
+            var url = host + "delete_channel";
+            uu_request.request(url, data, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    cb(err,body);
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
+        //更新渠道
+        update_channel: function(data,cb) {
+            var url = host + "update_channel";
+            uu_request.request(url, data, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    cb(err,body);
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
+        //保存渠道
+        save_channel: function(data,cb) {
+            var url = host + "save_channel";
+            uu_request.request(url, data, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    // var info = JSON.parse(body);
+                    cb(err,body);
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
+        //查询所有渠道
+        get_channels: function(cb) {
+            var url = host + "get_channels";
+            uu_request.get(url, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    cb(err,JSON.parse(body));
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
+        //更新部门
+        update_department: function(data,cb) {
+            var url = host + "update_department";
+            uu_request.request(url, data, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    cb(err,body);
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
+        //根据id查找部门
+        search_department_byId: function(id,cb) {
+            var url = host + "search_department_byId?id=" + id;
+            uu_request.get(url, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    cb(err,JSON.parse(body));
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
+        //删除部门
+        delete_department: function(data,cb) {
+            var url = host + "delete_department";
+            uu_request.request(url, data, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    cb(err,body);
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
+        //保存部门
+        save_department: function(data,cb) {
+            var url = host + "save_department";
+            uu_request.request(url, data, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    // var info = JSON.parse(body);
+                    cb(err,body);
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
         //查询部门
         get_departments: function(cb) {
             var url = host + "get_departments";
@@ -33,7 +157,6 @@ var nav = function(server) {
                 }
             });
         },
-
 
     };
 };
